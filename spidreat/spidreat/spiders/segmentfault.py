00c30d22593a8f5ed0ae68872bad9e5894f7d587
kasheemlew <417879751@qq.com>
2016-10-15 21:09:58 +0800

    updated

--- spidreat/spidreat/spiders/segmentfault.py
+++ spidreat/spidreat/spiders/segmentfault.py
@@ -5,7 +5,7 @@ import scrapy
 from spidreat.items import SegmentFaultItem
 
 
-class SegmentFaultSpider(scrapy.Spider):
+class SegmentFaultPythonSpider(scrapy.Spider):
     name = "segmentfaultpython"
     allowed_domains = ["segmentfault.com"]
     start_urls = [
@@ -13,9 +13,14 @@ class SegmentFaultSpider(scrapy.Spider):
             ]
 
     def parse(self, response):
-        for each in 
+        for each in response.xpath('//div[@class="summary"]'):
             try:
-
+                time = each.xpath('ul/li/text()').extract()[3].strip()
+                item = SegmentFaultItem()
+                item['title'] = each.xpath('h2/a/text()').extract()
+                item['link'] = ''.join(['https://segmentfault.com/', each.xpath('h2/a/@href').extract()[0]])
+                item['timestamp'] = time
+                item['tag'] = 'SegmentFaultPython'
                 yield item
             except IndexError:
                 continue
